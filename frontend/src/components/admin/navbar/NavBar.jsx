6b44724e18391a1bf535f273297ae5484f26af92
Akshay kumar k <akshayajith40@gmail.com>
2023-03-30 21:14:04 +0530

    table responsiveness bug fixed

--- frontend/src/components/admin/navbar/NavBar.jsx
+++ frontend/src/components/admin/navbar/NavBar.jsx
@@ -1,25 +1,39 @@
-import React from 'react'
+import React from "react";
 
 function NavBar() {
+  const menus = [
+    {
+      name: "Dashboard",
+      src: "/image/Chart.png",
+    },
+    {
+      name: "Add Vendors",
+      src: "/image/folder.png",
+    },
+  ];
   return (
     <>
-    <div className='flex'>
-      <div className='bg-dark-purple h-screen p-5  pt-8 sm:w-72 w-25 '>
-      <div className='sm:pl-6 pl-2 sm:text-2xl text-lg text-white'>
-        <h1>Admin</h1>
+      <div className="flex">
+        <div className="bg-dark-purple h-screen p-5  pt-8 md:w-72 w-24 ">
+          <div className="md:pl-6 pl-2 md:text-2xl text-lg text-white">
+            <h1>Admin</h1>
+          </div>
+          <ul className="pt-12">
+            {menus.map((ele, index) => (
+              <li className="flex rounded-md p-4 cursor-pointer hover:bg-light-white text-gray-300 text-lg items-center">
+                <img
+                  src={ele.src}
+                  alt="text"
+                  className="hover:bg-light-white"
+                />
+                <span className="hidden md:block px-2">{ele.name}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <ul className='pt-12'>
-        <li className='flex rounded-md p-4 cursor-pointer hover:bg-light-white text-gray-300 text-lg items-center   justify-center'>
-          <img src="/image/Chart.png" alt="text" className='hover:bg-light-white'/><span className='hidden sm:block px-2'>Dashboard</span>
-        </li>
-        <li className='flex rounded-md p-4 cursor-pointer hover:bg-light-white text-gray-300 text-lg items-center  justify-center'>
-          <img src="/image/folder.png" alt="text" className='hover:bg-light-white' /><span className='hidden sm:block px-2'>Dashboard</span>
-        </li>
-      </ul>
-      </div>
-    </div>
     </>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
--- frontend/src/components/admin/view_vendors/ViewVendors.jsx
+++ frontend/src/components/admin/view_vendors/ViewVendors.jsx
@@ -6,20 +6,22 @@ function ViewVendors() {
       <div className="p-6 text-2xl font-bold">
         <h1>Vendors</h1>
       </div>
-      <div className="flex justify-center items-center px-6 pt-4">
+      <div className="md:flex justify-center md:items-center md:px-6 pt-4 px-2 overflow-x-auto">
         <table className="table-fixed border min-w-full">
           <thead className="border-b font-medium">
             <tr className="border-r">
-              <th className="border px-6 py-4">hi</th>
-              <th className="border px-6 py-4">hi</th>
-              <th className="border px-6 py-4">hello</th>
+              <th className="border px-6 py-4">Sl.No</th>
+              <th className="border px-6 py-4">Name</th>
+              <th className="border px-6 py-4">Email</th>
+              <th className="border px-6 py-4">Phone</th>
             </tr>
           </thead>
           <tbody>
             <tr className="border-b">
-              <th className="border px-6 py-4 font-normal">hi</th>
-              <th className="border px-6 py-4 font-normal">hi</th>
-              <th className="border px-6 py-4 font-normal">hello</th>
+              <th className="border px-6 py-4 font-normal">1</th>
+              <th className="border px-6 py-4 font-normal">AkshayKumarK</th>
+              <th className="border px-6 py-4 font-normal">akshayajith05@gmail.com</th>
+              <th className="border px-6 py-4 font-normal">9744549400</th>
             </tr>
           </tbody>
         </table>
