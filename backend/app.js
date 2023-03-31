a76b9e6944566e35081aea022d99685c4eade0dc
Akshay kumar k <akshayajith40@gmail.com>
2023-03-31 16:15:24 +0530

    user Schema created

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const dbconnect = require('./config/connections');
+const userRoutes = require('./routes/userRoutes');
+const adminRoutes = require('./routes/adminRoutes');
 
 dotenv.config();
 const app = express();
@@ -10,6 +12,9 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 dbconnect.dbconnect();
+
+app.use('/', userRoutes);
+app.use('/admin', adminRoutes);
 
 app.listen(process.env.PORTNO || 8000, () => {
   console.log('server started listening to port');
---
+++ backend/model/userSchema.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  bussinessName: {
+    type: String,
+    required: true,
+  },
+});
+
+const user = mongoose.model('users', userSchema);
+module.exports = user;
