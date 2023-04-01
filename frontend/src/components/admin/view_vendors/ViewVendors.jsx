8313a428d33b1efb2fb233428b2be97943f40b1a
Akshay kumar k <akshayajith40@gmail.com>
2023-04-01 18:06:09 +0530

    vendor details shown in admin side when clicking on the respective row completed

--- frontend/src/components/admin/view_vendors/ViewVendors.jsx
+++ frontend/src/components/admin/view_vendors/ViewVendors.jsx
@@ -1,8 +1,23 @@
 import React, { useEffect, useState } from "react";
 import axios from "../../../axios/AdminAxios";
+import { Modal } from "antd";
 
 function ViewVendors() {
   const [vendors, setVendors] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedVendor, setSelectedVendor] = useState(null);
+  const showModal = (vendorId) => {
+    setSelectedVendor(vendorId);
+    setIsModalOpen(true);
+  };
+  const handleCancel = () => {
+    setSelectedVendor(null);
+    setIsModalOpen(false);
+  };
+  const handleOk = () => {
+    setSelectedVendor(null);
+    setIsModalOpen(false);
+  };
   useEffect(() => {
     axios.get("/admin/vendors").then((response) => {
       setVendors(response.data.vendors);
@@ -24,21 +39,61 @@ function ViewVendors() {
             </tr>
           </thead>
           <tbody>
-              {vendors.map((ele, index) => (
-            <tr className="border-b" key={ele._id}>
-                  <th className="border px-6 py-4 font-normal" >
-                    {index + 1}
-                  </th>
-                  <th className="border px-6 py-4 font-normal">{ele.firstName + ele.lastName}</th>
-                  <th className="border px-6 py-4 font-normal">
-                    {ele.email}
-                  </th>
-                  <th className="border px-6 py-4 font-normal">{ele.phone}</th>
-            </tr>
-              ))}
+            {vendors.map((ele, index) => (
+              <tr
+                className="border-b"
+                key={ele._id}
+                onClick={() => showModal(ele._id)}
+              >
+                <th className="border px-6 py-4 font-normal">{index + 1}</th>
+                <th className="border px-6 py-4 font-normal">
+                  {ele.firstName + ele.lastName}
+                </th>
+                <th className="border px-6 py-4 font-normal">{ele.email}</th>
+                <th className="border px-6 py-4 font-normal">{ele.phone}</th>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
+      {selectedVendor && (
+        <Modal
+          title="Add Vendor"
+          open={isModalOpen}
+          onOk={handleOk}
+          onCancel={handleCancel}
+          closable={false}
+          footer={null}
+          okButtonProps={{
+            style: { background: "#007cff", color: "white" },
+          }}
+        >
+          <div className="border border-gray-200 sm:m-10 flex justify-around h-28 items-center">
+            <h2 className="font-bold">
+              Service:{" "}
+              <span className="font-normal">
+                {
+                  vendors.find((vendor) => vendor._id === selectedVendor)
+                    .bussinessName
+                }
+              </span>
+            </h2>
+            <img
+              src={`http://localhost:8000/bussiness_images/${selectedVendor}.jpg`}
+              alt=""
+              className="w-32"
+            />
+          </div>
+          <div className="flex justify-end">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={handleOk}
+            >
+              OK
+            </button>
+          </div>
+        </Modal>
+      )}
     </div>
   );
 }
